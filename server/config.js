30e498b2b1a2c0bd69c86f6d30be0a6a455b6a1f
JAILBREAK-101 <samuelifeoluwa540@gmail.com>
2024-10-26 15:34:13 +0100

    connect MongoDB and server

---
+++ server/config.js
@@ -0,0 +1,27 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const URI = process.env.MONGO_URI || "";
+const client = new MongoClient(URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function main() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();
+
+let connectDB = client.db("employees");
+module.exports = connectDB;
