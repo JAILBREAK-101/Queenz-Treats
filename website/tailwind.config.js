f5d16355637d0c7fb4514c9192b829fa2b54ac1b
JAILBREAK-101 <samuelifeoluwa540@gmail.com>
2025-01-15 14:06:25 +0100

    chore: configure postcss build for styles

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
