d782a8b8e77ee5a4b4d299c64de5faa90de2ed74
JAILBREAK-101 <samuelifeoluwa540@gmail.com>
2025-01-16 21:52:34 +0100

    commit

---
+++ server/server.js
@@ -0,0 +1,8 @@
+// This Server Side Code is needed for the following sections of the website:
+
+/* 
+    - Hero Section Images
+    - Contact Form Submission and Email Processing For The Start Planning CTA.
+    - Our Dishes Section rendering different dishes that once clicked leads to a dedicated page for the dishes or rather the contact form for Order (Integrated with a payment system for getting those dishes) - The Schema for the dishes: Picture, Name, Description and Price (In Naira) Or Country based on location
+*/
+
