a95aeb8ea038d03bd80405c924845367f5788b16
JAILBREAK-101 <samuelifeoluwa540@gmail.com>
2025-01-22 16:17:40 +0100

    refactor(client): remove unused files and configurations, including .gitignore, ESLint config, HTML, package files, and various components

--- client/src/shared/components/loaders/LoadingError.tsx
+++
@@ -1,8 +0,0 @@
-export const LoadingError = (props: { display: string | null, showLogout?: boolean }) => {
-    
-    return (
-        <section className="text-center" style={{paddingTop: '5vh'}}>
-            <h4 className="text-red-500 font-bold">{props.display}</h4>
-        </section>
-    );
-};
--- client/src/shared/components/loaders/SuccessDisplay.tsx
+++
@@ -1,8 +0,0 @@
-export const SuccessDisplay = (props: { display: string | null, showLogout?: boolean }) => {
-
-    return (
-        <section className="text-center" style={{paddingTop: '2vh'}}>
-            <h4 className="text-green-600 font-bold">{props.display}</h4>
-        </section>
-    );
-};
